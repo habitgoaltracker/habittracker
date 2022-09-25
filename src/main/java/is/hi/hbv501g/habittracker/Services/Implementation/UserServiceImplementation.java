a18988c09ea64b5bde8c2d4979f6d28080649b14
arnibjo <abo25@hi.is>
2022-09-25 20:04:02 +0000

    Packages

---
+++ src/main/java/is/hi/hbv501g/habittracker/Services/Implementation/UserServiceImplementation.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.habittracker.Services.Implementation;
+
+import is.hi.hbv501g.habittracker.Services.UserService;
+
+/**************************************************************
+
+ nafn: Árni Björn Óskarsson
+ t-póstur: abo25@gmail.com
+
+ lýsing:
+ **************************************************************/
+public class UserServiceImplementation implements UserService {
+}
---
+++ src/main/java/is/hi/hbv501g/habittracker/Services/UserService.java
@@ -0,0 +1,11 @@
+package is.hi.hbv501g.habittracker.Services;
+
+/**************************************************************
+
+ nafn: Árni Björn Óskarsson
+ t-póstur: abo25@gmail.com
+
+ lýsing:
+ **************************************************************/
+public interface UserService {
+}
