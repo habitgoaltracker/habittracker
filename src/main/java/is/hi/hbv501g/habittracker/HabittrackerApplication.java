8c5ef9fb675f7d72d38993f7265fed2318c37416
Sakkinn <isakmar00@gmail.com>
2022-09-25 18:46:49 +0000

    Initial commit.

---
+++ src/main/java/is/hi/hbv501g/habittracker/HabittrackerApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabittrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabittrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/habittracker/HabittrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
