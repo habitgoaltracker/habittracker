dd81f1fa2ed79d985cfe228e68b9604dc8c736b5
arnibjo <abo25@hi.is>
2022-09-25 19:00:20 +0000

    commit one

--- src/main/java/is/hi/hbv501g/habittracker/HabittrackerApplication.java
+++ src/main/java/is/hi/hbv501g/habittracker/HabittrackerApplication.java
@@ -2,7 +2,7 @@ package is.hi.hbv501g.habittracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this is team member Árni Björn
 @SpringBootApplication
 public class HabittrackerApplication {
 
